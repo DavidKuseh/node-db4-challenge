cb7fa0a18b8ea3bcf9ec15ecf1d002221c30c799
David Kuseh <davegru203@gmail.com>
2019-11-07 16:27:06 +0000

    created migrations and seeds

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,12 @@ module.exports = {
     directory: './data/migrations'
   },
   seeds: {
-    directory: './data/seeds'
-  }
+    directory: './seeds'
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      // runs after a connection is made to the sqlite engine
+      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    },
+  },
 };
